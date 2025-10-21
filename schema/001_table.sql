457245039a5551257819e5eb91341648aec0dd0e
Tolben <vitek.pasek@gmail.com>
2025-10-21 02:35:52 +0200

    chore(schema): split from umbrella

---
+++ schema/001_table.sql
@@ -0,0 +1,7 @@
+-- Auto-generated from schema-map.psd1 on 2025-10-21T02:32:05
+-- table: countries
+CREATE TABLE IF NOT EXISTS countries (
+  iso2 CHAR(2) PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  CONSTRAINT chk_countries_iso2 CHECK (iso2 REGEXP ''^[A-Z]{2}$'')
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;

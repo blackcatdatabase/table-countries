130306a240a0a98352de9232e6a7fd068a76f00d
Tolben <vitek.pasek@gmail.com>
2025-10-22 20:22:47 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/001_table.sql
+++ schema/001_table.sql
@@ -3,5 +3,5 @@
 CREATE TABLE IF NOT EXISTS countries (
   iso2 CHAR(2) PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
-  CONSTRAINT chk_countries_iso2 CHECK (iso2 REGEXP ''^[A-Z]{2}$'')
+  CONSTRAINT chk_countries_iso2 CHECK (iso2 REGEXP '^[A-Z]{2}$')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
